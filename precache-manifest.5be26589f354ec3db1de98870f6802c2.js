8d0773c94255cf64641a2ba3cc27d1719d41f95e
ccharfu <carlosuefagh@gmail.com>
2020-05-09 17:52:29 -0500

    Updates

---
+++ precache-manifest.5be26589f354ec3db1de98870f6802c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b69f0b53a02cc53d4fb0cbd58cdb25e",
+    "url": "/bienestarapp/index.html"
+  },
+  {
+    "revision": "02330791aa809847ae68",
+    "url": "/bienestarapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ffe24522c2a71cc24b7f",
+    "url": "/bienestarapp/static/js/2.5cdb4892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bienestarapp/static/js/2.5cdb4892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02330791aa809847ae68",
+    "url": "/bienestarapp/static/js/main.909add78.chunk.js"
+  },
+  {
+    "revision": "d2542e8951a6e08d6b6e",
+    "url": "/bienestarapp/static/js/runtime-main.b0d6540a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bienestarapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
